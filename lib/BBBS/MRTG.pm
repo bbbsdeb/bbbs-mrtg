333de83001ff62484ba64af0368080e4d8901c09
RJ Clay <jame@rocasa.us>
2012-02-11 11:55:35 -0500

    Bump version to v0.03

--- lib/BBBS/MRTG.pm
+++ lib/BBBS/MRTG.pm
@@ -9,11 +9,11 @@ BBBS::MRTG - Perl Module for reading the MRTG information from the BBBS standard
 
 =head1 VERSION
 
-Version 0.02
+Version 0.03
 
 =cut
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 
 =head1 SYNOPSIS
